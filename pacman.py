38494a6f9fc08cdba8bc6de9577285f3f7860f88
Memocana <Memo@Memows-MacBook-Air.local>
2015-02-06 09:56:10 +0100

    Revert 396a87a..7d90a83
    This rolls back to commit 396a87a90c79e5b0be20ff3b94dbaca702fd3e18.

--- pacman.py
+++ pacman.py
@@ -203,10 +203,10 @@ class Ghosts: # Default Blinky behaviour, follows pacman where ever he goes
 				elif counter == 3:
 					dist = self.calculateRoute(self.x, self.y-1, target)
 					directionToMove = -2
-			if directionToMove != 0 and (dist < minDistance or minDistance == -1) and (target != "run"): # Min distance to
+			if directionToMove != 0 and (dist < minDistance or minDistance == -1) and (target != "run"):
 				minDistance = dist
 				idealDirection = directionToMove
-			elif directionToMove != 0 and dist > minDistance: #Max distance to PacMan
+			elif directionToMove != 0 and dist > minDistance and target == "run":
 				minDistance = dist
 				idealDirection = directionToMove
 			counter += 1
